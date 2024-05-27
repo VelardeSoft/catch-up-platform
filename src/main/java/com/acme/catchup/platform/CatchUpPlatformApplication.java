9ca9b2b5435fd2ba3d65b7233417d42074d03f93
VelardeSoft <u20211c221@upc.edu.pe>
2024-05-27 18:05:40 -0500

    feat: Agregamos algunos carpetas.

--- src/main/java/com/acme/catchup/platform/CatchUpPlatformApplication.java
+++ src/main/java/com/acme/catchup/platform/CatchUpPlatformApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CatchUpPlatformApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CatchUpPlatformApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CatchUpPlatformApplication.class, args);
+    }
 
 }
